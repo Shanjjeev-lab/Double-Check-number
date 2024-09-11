61ac5c0a285a55631612067b57ea854703b228a2
Shanjjeev-Lab <shanjjeevrohan2911@gmail.com>
2024-09-11 18:57:30 +0530

    Python Double-Check number

---
+++ main.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+
+if num > 50:
+    print(num, "Is greater than 50")
+    if num != 2 == 0:
+        print("Also it is EVEN")
+    else:
+        print("Also it is ODD")
+else:
+    print(num, "Is not greater than 50")
